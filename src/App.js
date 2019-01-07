87edc9c800f6a06875e2ae046fe6c7794372e4cb
Dylan Landon <dylan43landon@gmail.com>
2019-01-07 12:20:37 -0800

    assignment 2766 Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import React, { Component } from 'react';
-import RoomList from './components/RoomList';
-import './App.css';
-
 import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
+// Initialize Firebase
 
-var config = {
+// Initialize Firebase
+  var config = {
     apiKey: "AIzaSyBhtVa59rx03xYh9LuWiaaVpOy5pSz6H1U",
     authDomain: "bloc-chat-react-575f9.firebaseapp.com",
     databaseURL: "https://bloc-chat-react-575f9.firebaseio.com",
@@ -16,15 +16,68 @@ var config = {
   };
   firebase.initializeApp(config);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Chat App</h1>
-        <RoomList firebase= {firebase}/>
-      </div>
-    );
-  }
-}
+  class App extends Component {
+    constructor(props) {
+      super(props);
 
-export default App;
+      this.roomsRef = firebase.database().ref('rooms');
+
+      this.state = {
+        active: false,
+        rooms: [],
+        roomName: '',
+        user: 'Guest',
+        showForm: "display-none message-form"
+      };
+    }
+
+    setActive(e) {
+      this.setState({ active: e.key, showForm: "display-block message-form"});
+      this.roomTitle(e.key);
+
+    }
+
+    roomTitle(key) {
+      const activeRoomID = key;
+      const activeRoom = this.state.rooms.filter(room => room.key === activeRoomID);
+      this.setState({roomName: activeRoom[0].name });
+    }
+
+    setUser(e) {
+      if (e === null) {
+        this.setState({ user: 'Guest' })}
+      else {
+        this.setState({ user: e.displayName })
+      }
+    }
+
+    componentDidMount() {
+      this.roomsRef.on('child_added', snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat( room ) });
+      });
+    };
+
+    render() {
+      return (
+        <div className="App">
+          
+          <RoomList
+            firebase = {firebase}
+            setActive = {(e) => this.setActive(e)}
+            active = {this.state.active}
+          />
+          <MessageList
+            firebase = {firebase}
+            active = {this.state.active}
+            roomName = {this.state.roomName}
+            user = {this.state.user}
+            showForm = {this.state.showForm}
+          />
+        </div>
+      );
+    }
+  }
+
+  export default App;
